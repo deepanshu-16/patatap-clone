6abd5f9f39e3fc8957d2daa4211333492273dfa5
Deepanshu Narang <30462445+deepanshu-16@users.noreply.github.com>
2019-08-10 23:24:07 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,158 @@
+var keyData = {
+	a : {
+		color: "#FF0000",
+		sound: new Howl({
+      		src: ['sounds/bubbles.mp3']
+    	}),
+	},
+	b : {
+		color: "#800000",
+		sound: new Howl({
+      		src: ['sounds/clay.mp3']
+    	}),
+	},
+	c : {
+		color: "#FFFF00",
+		sound: new Howl({
+      		src: ['sounds/confetti.mp3']
+    	}),
+	},
+	d : {
+		color: "#808000",
+		sound: new Howl({
+      		src: ['sounds/corona.mp3']
+    	}),
+	},
+	e : {
+		color: "#00FF00",
+		sound: new Howl({
+      		src: ['sounds/dotted-spiral.mp3']
+    	}),
+	},
+	f : {
+		color: "#008000",
+		sound: new Howl({
+      		src: ['sounds/flash1.mp3']
+    	}),
+	},
+	g : {
+		color: "#00FFFF",
+		sound: new Howl({
+      		src: ['sounds/flash2.mp3']
+    	}),
+	},
+	h : {
+		color: "#008080",
+		sound: new Howl({
+      		src: ['sounds/flash3.mp3']
+    	}),
+	},
+	i : {
+		color: "#0000FF",
+		sound: new Howl({
+      		src: ['sounds/glimmer.mp3']
+    	}),
+	},
+	j : {
+		color: "#000080",
+		sound: new Howl({
+      		src: ['sounds/moon.mp3']
+    	}),
+	},
+	k : {
+		color: "#FF00FF",
+		sound: new Howl({
+      		src: ['sounds/pinwheel.mp3']
+    	}),
+	},
+	l : {
+		color: "#FF0023",
+		sound: new Howl({
+      		src: ['sounds/piston-1.mp3']
+    	}),
+	},
+	m : {
+		color: "#809200",
+		sound: new Howl({
+      		src: ['sounds/piston-2.mp3']
+    	}),
+	},
+	n : {
+		color: "#F25F00",
+		sound: new Howl({
+      		src: ['sounds/piston-3.mp3']
+    	}),
+	},
+	o : {
+		color: "#802590",
+		sound: new Howl({
+      		src: ['sounds/prism-1.mp3']
+    	}),
+	},
+	p : {
+		color: "#ff4F90",
+		sound: new Howl({
+      		src: ['sounds/prism-2.mp3']
+    	}),
+	},
+	q : {
+		color: "#f080f0",
+		sound: new Howl({
+      		src: ['sounds/prism-3.mp3']
+    	}),
+	},
+	r : {
+		color: "#00F88F",
+		sound: new Howl({
+      		src: ['sounds/splits.mp3']
+    	}),
+	},
+	s : {
+		color: "#f08080",
+		sound: new Howl({
+      		src: ['sounds/squiggle.mp3']
+    	}),
+	},
+	t : {
+		color: "#f08999",
+		sound: new Howl({
+      		src: ['sounds/strike.mp3']
+    	}),
+	},
+	u : {
+		color: "#FF66FF",
+		sound: new Howl({
+      		src: ['sounds/suspension.mp3']
+    	}),
+	},
+	v : {
+		color: "#1F2090",
+		sound: new Howl({
+      		src: ['sounds/timer.mp3']
+    	}),
+	},
+	w : {
+		color: "#123456",
+		sound: new Howl({
+      		src: ['sounds/ufo.mp3']
+    	}),
+	},
+	x : {
+		color: "#8765ff",
+		sound: new Howl({
+      		src: ['sounds/veil.mp3']
+    	}),
+	},
+	y : {
+		color: "#160499",
+		sound: new Howl({
+      		src: ['sounds/wipe.mp3']
+    	}),
+	},
+	z : {
+		color: "#879898",
+		sound: new Howl({
+      		src: ['sounds/zig-zag.mp3']
+    	}),
+	}
+}
